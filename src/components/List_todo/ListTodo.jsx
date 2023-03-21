5d181b4c455183791b02c6d72df4aa9776dcb05b
tolani onabiyi <tolani@Tolanis-MacBook-Air.local>
2023-03-21 20:30:54 +0000

    Added task list component

--- src/components/List_todo/ListTodo.jsx
+++ src/components/List_todo/ListTodo.jsx
@@ -8,6 +8,16 @@ export default function ListTodo() {
         <div className="listTodo__item">Buy New Laptop</div>
         <div className="listTodo__delete">Del</div>
       </div>
+
+      <div className="listTodo__row">
+        <div className="listTodo__item">Make Codehance Course </div>
+        <div className="listTodo__delete">Del</div>
+      </div>
+
+      <div className="listTodo__row">
+        <div className="listTodo__item">Complete Today's Task </div>
+        <div className="listTodo__delete">Del</div>
+      </div>
     </div>
   );
 }
